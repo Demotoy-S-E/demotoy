6e0856a57d11664315d879cbf0fc26ffb6e5d95d
Asier <asier.marin@opendeusto.es>
2020-11-29 13:08:34 +0100

    metodos clientes

--- rpiBroker/servicios/clienteRPI.py
+++ rpiBroker/servicios/clienteRPI.py
@@ -2,4 +2,4 @@ from servicios.weblogging import Applogging
 class clienteRPI:
 
     def __init__(self, nombre_log):
-        self.__cliente1_log = Applogging(nombre_log)
+        self.__cliente_log = Applogging(nombre_log)
--- rpiBroker/startup.py
+++ rpiBroker/startup.py
@@ -31,6 +31,8 @@ class Startup:
             self.sesion = self.__servicio_db.sesion
             self.__log_startup.info_log("Creando tablas")
             usuario.Base.metadata.create_all(bind = self.__servicio_db.engine)
+            self.sesion.commit()
+            self.sesion.close()
         except:
             self.__log_startup.error_log("Error a la hora de crear tablas")
 
