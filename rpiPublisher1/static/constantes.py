94a56d76a8fa6ca77c5b519b9d55a892f0c325f3
aritz <aritzibaest@opendeusto.es>
2020-12-28 22:17:03 +0100

    .

---
+++ rpiPublisher1/static/constantes.py
@@ -0,0 +1,14 @@
+# Url views statics
+TEMPLATE_INDEX_CONSTANTE = '/index.html'
+TEMPLATE_REGISTRO_CONSTANTE = '/registro.html'
+TEMPLATE_PRINCIPAL_CONSTANTE = '/principal.html'
+
+# Url directions
+DIRECCION_PRINCIPAL_CONSTANTE = '/principal'
+DIRECCION_INDEX_CONSTANTE = '/'
+
+# Rpi statics
+SECUANCIA_SEGUNDOS_RPI = 10
+
+# Rpi pins
+BUZZER_PIN = 17
