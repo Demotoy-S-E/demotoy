b5f8fbe9dec790a01bc148bc8925224f4e5a2e52
Asier <asier.marin@opendeusto.es>
2021-01-07 03:35:45 +0100

    cliente mqtt con hilo de procesamiento

--- rpiBroker/servicios/clienteRPI1.py
+++ rpiBroker/servicios/clienteRPI1.py
@@ -9,6 +9,11 @@ except:
     SIMULACION = True
 
 import json
+import subprocess
+import os
+import time
+import threading
+import atexit
 from servicios.mqtt import Mqtt
 from servicios.weblogging import Applogging
 from comun.singleton import Singleton
@@ -25,39 +30,37 @@ class ClienteRPI1:
 
     def __init__(self, nombre_log):
         self.__cliente_log = Applogging(nombre_log)
+        self.__hilo_datalock = threading.Lock()
+        self.__hilo_cliente_rpi = threading.Thread()
+        self.__comenzar_servicio_background()
+            
+    def __comenzar_servicio_background(self):
         global SIMULACION
         if (SIMULACION == False):
             self.client = mqtt.Client()
             self.__cliente_log.info_log(f"Configuracion client rpi1 {TOPIC_ACELEROMETRO}:{HOSTNAME_SIMULACION_LOCAL}")
-            # super(ClienteRPI1, self).__init__(
-                # cliente = self.cliente, 
-                # topic = TOPIC_ACELEROMETRO,
-                # hostname = HOSTNAME_SIMULACION_LOCAL)
-            self.client.on_connect = _on_connect
-            self.client.on_message = _on_message
+            self.client.on_connect = self._on_connect
+            self.client.on_message = self._on_message
             self.client.connect(HOSTNAME_SIMULACION_LOCAL, 1883, 60)
+        self.__hilo_cliente_rpi = threading.Timer(0, self.__obtener_datos_cliente_mqtt, ())
+        self.__cliente_log.info_log("Servicio cliente mqtt 1 en el background...")
+        self.__hilo_cliente_rpi.start()
+
+    def __obtener_datos_cliente_mqtt(self):
+        self.__hilo_cliente_rpi = threading.Timer(0.1, self.__obtener_datos_cliente_mqtt, ())
+        if (SIMULACION == False):   
             self.client.loop_forever()
+        with self.__hilo_datalock:
+            self.__hilo_cliente_rpi.start()
     
-def _on_connect(client, serdata, flags, rc):
-        global TOPIC_ACELEROMETRO
-        print("Connected with result code "+str(rc))
+    def _on_connect(self, client, serdata, flags, rc):
+            global TOPIC_ACELEROMETRO
+            self.__cliente_log.info_log("Conectado como cliente mqtt, con codigo: " + str(rc))
+            client.subscribe(TOPIC_ACELEROMETRO)
+
+    def _on_message(self, client, userdata, msg):
+            msg.payload = msg.payload.decode("utf-8")
+            mensaje_recibido = msg.payload
+            self.__cliente_log.info_log(msg.topic + " "+ mensaje_recibido)
+            mensaje_recibido_json = json.loads(msg.payload )
     
-        # Nos subscribirmos al topic 
-        client.subscribe(TOPIC_ACELEROMETRO)
-
-def _on_message(client, userdata, msg):
-        msg.payload = msg.payload.decode("utf-8")
-        mensaje_recibido = msg.payload
-        print(msg.topic+" "+mensaje_recibido)
-
-        #the message received starts with 'b, that mean bytes. 
-        mensaje_recibido_json =json.loads(msg.payload )
-    
-        varx=mensaje_recibido_json["varx"]
-        print(varx)
-
-        vary=mensaje_recibido_json["vary"]
-        print(vary)
-
-        varz=mensaje_recibido_json["varz"]
-        print(varz)
